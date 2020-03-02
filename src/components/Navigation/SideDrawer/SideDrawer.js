3faa08e579b047339613e084b619105e899fbb38
Amar Nath Yogi <immortal2907@gmail.com>
2020-03-03 01:05:28 +0530

    Added SideDrawer Component & DrawerToggle Button

---
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Logo from "../../Logo/Logo";
+import NavigationItems from "../NavigationItems/NavigationItems";
+import classes from "./SideDrawer.module.css";
+import Backdrop from "../../UI/Backdrop/Backdrop";
+
+const SideDrawer = props => {
+  return (
+    <>
+      <Backdrop show={props.show} clicked={props.closed} />
+      <div
+        className={[
+          classes.SideDrawer,
+          props.show ? classes.Open : classes.Close
+        ].join(" ")}
+      >
+        <div className={classes.Logo}>
+          <Logo />
+        </div>
+        <nav>
+          <NavigationItems />
+        </nav>
+      </div>
+    </>
+  );
+};
+
+export default SideDrawer;
